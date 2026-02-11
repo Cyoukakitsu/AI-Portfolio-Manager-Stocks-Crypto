fa46ac2097dd5ad083cfb51e1823cf7245796ff8
ZHAO JIAYI <z261459808@gmail.com>
2026-02-11 21:51:14 +0900

    feat(auth):implement sign in page UI

--- components/custom/auth/sign-up-form.tsx
+++ components/custom/auth/sign-up-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useForm } from "react-hook-form";
-import { SignUpFormValues, signUpSchema } from "@/lib/schemas/sign-up-schema";
+import { SignUpFormValues, signUpSchema } from "@/lib/schemas/sign-up";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import {

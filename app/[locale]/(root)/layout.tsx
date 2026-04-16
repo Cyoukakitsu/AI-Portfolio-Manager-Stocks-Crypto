8f664741fecf6c1f946adb51030d91ecb68b66dd
ZHAO JIAYI <z261459808@gmail.com>
2026-04-16 21:52:15 +0900

    feat: remove HeaderControls from root layout (moved to NavBar)

--- app/[locale]/(root)/layout.tsx
+++ app/[locale]/(root)/layout.tsx
@@ -1,17 +1,9 @@
 import React from "react";
-import { HeaderControls } from "@/features/dashboard/components/header-controls";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="relative">
-      <header className="absolute top-0 right-0 flex h-14 items-center px-4 z-10">
-        <HeaderControls />
-      </header>
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
